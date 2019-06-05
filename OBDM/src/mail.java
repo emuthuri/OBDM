87bf7f7d3107316194ef6cae858126193b84fe0d
fourthexit <ericmwesh@gmail.com>
2019-06-05 14:56:22 +0300

    First Commit

--- OBDM/src/mail.java
+++ OBDM/src/mail.java
@@ -6,4 +6,8 @@ public static void main(String[]args){
 	System.out.println("Hello world");
 	System.out.println(7*7);
 	}
+
+public void second() {
+	//just because we want to ensure tracked changes
+}
 }
